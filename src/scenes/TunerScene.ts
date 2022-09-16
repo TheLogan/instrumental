752395ee16ac83862fe846ede7a539c7740f4989
Jacob Collstrup <jacob@collstrup.dk>
2022-09-16 18:50:19 +0200

    Added simple stuff to show note

--- src/scenes/TunerScene.ts
+++ src/scenes/TunerScene.ts
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
 import TunerEngine from '../Utilities/TunerEngine';
+import { notes } from '../Utilities/Constants';
+
 export default class TunerScene extends Phaser.Scene {
     constructor() {
         super('TunerScene');
@@ -13,15 +15,16 @@ export default class TunerScene extends Phaser.Scene {
         this.textRender = this.add.text(this.renderer.width / 2, this.renderer.height / 2, '', { fontSize: '64px' });
         this.tunerEngine = new TunerEngine();
     }
-    
+
 
     update() {
         //console.log('update');
         const freq = this.tunerEngine?.getFreq();
-        console.log('freq',freq);
+        let noteIndex = notes.findIndex(x => freq && x.freq > freq);
+        console.log('freq', notes[noteIndex].note, freq);
         
-        if(this.textRender)
-            this.textRender.text = String(freq);
+        if (noteIndex != -1 && this.textRender)
+            this.textRender.text = String(notes[noteIndex].note);
     }
 
 }
