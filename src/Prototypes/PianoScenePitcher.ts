843ce0b239f28c04ff42fba1e3381abbc6e09d28
Jacob Collstrup <jacob@collstrup.dk>
2022-09-17 21:41:15 +0200

    player now handles rests and ties

--- src/Prototypes/PianoScenePitcher.ts
+++ src/Prototypes/PianoScenePitcher.ts
@@ -1,9 +1,12 @@
 import Phaser from 'phaser'
-import OdeToJoy from "../Music/OdeToJoy.json";
+// import OdeToJoy from "../Music/OdeToJoy.json";
+import music from "../Music/SmokeOnTheWater.json";
 import { notesOld, noteLength, notes } from '../Utilities/Constants';
-import { dotLength } from '../Utilities/MusicMath';
+import { dotLength, tieLength } from '../Utilities/MusicMath';
 // import Pitcher from '../Utilities/Pitcher';
 import PitcherSampler from '../Utilities/PitcherSampler';
+
+
 
 export default class PianoScenePitcher extends Phaser.Scene {
   audioContex = new AudioContext();
@@ -17,18 +20,30 @@ export default class PianoScenePitcher extends Phaser.Scene {
   }
 
   create = () => {
-    this.playMelody();
+    this.playMelody(music);
   }
 
-  playMelody = () => {
+  playMelody = (music:any) => {
     const musicNotes: { timestamp: number, note: string, length: number }[] = [];
-    this.tempo = OdeToJoy['metronome mark'];
+    this.tempo = music['metronome mark'];
 
-    for (const event of OdeToJoy.events) {
+    let skipCount = 0;
+    for (const [index, event] of music.events.entries()) {
+      if (skipCount > 0) {
+        skipCount--;
+        continue;
+      }
       if (event.type === 'note') {
         let length = noteLength[event.length];
         if (event.relations?.dot) {
           length = dotLength(length);
+        }
+        if (event.relations?.tie) {
+          skipCount = event.relations.tie;
+          const tiedNotes = music.events.slice(index + 1, index + event.relations.tie + 1);
+          const tiedLengths = tiedNotes.map((x:any) => noteLength[x.length]);
+
+          length = tieLength(length, tiedLengths);
         }
         const lastTimestamp = musicNotes[musicNotes.length - 1]?.timestamp || 0;
         const lastLength = musicNotes[musicNotes.length - 1]?.length || 0;
@@ -47,7 +62,7 @@ export default class PianoScenePitcher extends Phaser.Scene {
         if (!currentNote) return;
         // this.pitcher.playPitch(notesOld[currentNote.note], this.ovtNumber, currentNote.length);
         let midi = notes.find(note => currentNote && note.note === currentNote.note)?.midi;
-        if(!midi) return;
+        if (!midi) return;
         this.pitcher.playSample(midi, currentNote.length);
       } else if (musicNotes.length <= 0) {
         clearInterval(musicInterval);
