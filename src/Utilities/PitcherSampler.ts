8365334db1ef8995c36414ef98666a9a95345849
Jacob Collstrup <jacob@collstrup.dk>
2022-09-17 17:11:57 +0200

    Added permanent distortion to pitchersampler

--- src/Utilities/PitcherSampler.ts
+++ src/Utilities/PitcherSampler.ts
@@ -10,14 +10,14 @@ export default class PitcherSampler {
 
     async initialize() {
         var source;
-        
+
         try {
             let resp = await fetch('assets/Samples/ac-gtr-str3.wav');
             this.sample = await this.audioContext.decodeAudioData(await resp.arrayBuffer());
         } catch (error) {
             throw error;
         }
-          
+
         if (!navigator?.mediaDevices?.getUserMedia) {
             // No audio allowed
             alert('Sorry, getUserMedia is required for the app.')
@@ -35,18 +35,36 @@ export default class PitcherSampler {
             }
         }
     }
-    playSample(noteToPlay:number, duration:number) {
-        if(!this.sample) return;
-        const gainNode = this.audioContext.createGain();
 
+    makeDistortionCurve(amount: number) {
+        const k = amount;
+        const n_samples = 44100;
+        const curve = new Float32Array(n_samples);
+        // const deg = Math.PI / 180;
+        let i = 0;
+        let x;
+        for (; i < n_samples; ++i) {
+            x = i * 2 / n_samples - 1;
+            curve[i] = (3 + k) * Math.atan(Math.sinh(x * 0.25) * 5) / (Math.PI + k * Math.abs(x));
+        }
+        return curve;
+    }
+
+
+    playSample(noteToPlay: number, duration: number) {
+        if (!this.sample) return;
+        const gainNode = this.audioContext.createGain();
+        const distNode = this.audioContext.createWaveShaper();
+        distNode.curve = this.makeDistortionCurve(400);
         const source = this.audioContext.createBufferSource();
         source.buffer = this.sample;
         source.playbackRate.value = 2 ** ((noteToPlay - 55) / 12);
-        source.connect(gainNode);
+        source.connect(distNode);
+        distNode.connect(gainNode);
         gainNode.connect(this.audioContext.destination);
-        gainNode.gain.setTargetAtTime(0, this.audioContext.currentTime + duration - 0.1, 0.5);
+        gainNode.gain.setTargetAtTime(0, this.audioContext.currentTime + duration - 0.2, 0.3);
         source.start(0);
         //source.stop(this.audioContext.currentTime + duration);
-      }
+    }
     //   playSample(sample, 60, 62);
 }
